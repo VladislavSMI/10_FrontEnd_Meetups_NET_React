1fb1ef7569df5db15acbee436edb9e93123c4a8d
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2025-04-01 15:59:46 +0200

    feat: create form components

---
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useField } from "formik";
+import { FormField, Label } from "semantic-ui-react";
+import DatePicker, { ReactDatePickerProps } from "react-datepicker";
+
+// Partial<ReactDatePickerProps> makes all props optional.
+// We don’t need to explicitly pass required props (like onChange) because Formik handles them internally via helpers.
+
+export default function MyDateInput(props: Partial<ReactDatePickerProps>) {
+  const [field, meta, helpers] = useField(props.name!);
+  return (
+    <FormField error={meta.touched && !!meta.error}>
+      <DatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={(value) => helpers.setValue(value)}
+      />
+      {meta.touched && meta.error ? (
+        <Label basic color="red">
+          {meta.error}
+        </Label>
+      ) : null}
+    </FormField>
+  );
+}
